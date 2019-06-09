731e6b15349be12878d97444aebb69519e406be5
hecasan <hetecaetano@gmail.com>
2019-06-08 23:43:09 -0300

    criado api in-memory-database

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,14 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDatabase implements InMemoryDbService{
+   createDb(){
+      const categories = [
+         { id: 1, name: 'Moradia', decription: 'Pagamento de Contas da casa'},
+         { id: 2, name: 'Saúde', decription: 'Plano de Saúde e Remédios'},
+         { id: 3, name: 'Lazer', decription: 'Cinema, praia, parques, etc'},
+         { id: 4, name: 'Salário', decription: 'Recebimento de Salários'},
+         { id: 5, name: 'Freelas', decription: 'Trabalhos como freelancer'},
+      ];
+      return { categories }
+   }
+}
